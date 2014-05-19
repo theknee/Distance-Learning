228ff089bec92eb927f11a2474895c93c056da2d
administrator <student@iMac-04.local>
2014-05-19 17:19:59 +0800

    modified server.js

--- source/server.js
+++ source/server.js
@@ -14,7 +14,7 @@ function start(route) {
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
+  console.log("Server has started at port 8888");
 }
 
 exports.start = start;
