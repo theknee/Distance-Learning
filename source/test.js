a31f6b40b012625e6672655f27a0723dfc8519d7
administrator <student@iMac-04.local>
2014-05-19 17:22:11 +0800

    added new file test(test script for linking html to node)

---
+++ source/test.js
@@ -0,0 +1,30 @@
+//== link html file to node js ==//
+
+var http = require('http');
+var fs = require('fs');
+
+var host = '127.0.0.1';
+var port = 1337;
+
+var server = http.createServer(function(request, response) {
+
+	console.log('read ' + request.url);
+
+	fs.readFile(',' + request.url , function(error, data) {
+
+		if (error){
+			response.writeHead(404, {'Content-type':'text/plain'});
+			response.end('404 file not found');
+		}
+		else{
+			response.writeHead(200, {'Content-type':'text/html'});
+			response.end(data);
+		}
+
+	});
+
+});
+
+server.listen(port, host, function() {
+	console.log('server listening at ' + host + ':' + port);
+});
