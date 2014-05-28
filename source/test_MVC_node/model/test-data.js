0fae6f5ab3f638a90b86beb080b35494e8b5025d
administrator <admin@iMac-05.local>
2014-05-28 12:13:41 +0800

    added test project MVC_node

---
+++ source/test_MVC_node/model/test-data.js
@@ -0,0 +1,6 @@
+var thelist = function() {
+ var objJson = {"GroupName":"D","count":4,"teams":[{"country":"England"}, {"country":"France"}, {"country":"Sweden"}, {"country":"Ukraine"}]};
+ return objJson;
+};
+
+exports.teamlist = thelist();
